82bf503bda43621a22dd9d99d5e30a6aa9ba87f9
fengye0319 <1084027066@qq.com>
2017-12-31 16:22:45 +0800

    Mod 完善首页相关javascript

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-
-var images = ["images/bannar-min-02.png", "images/bannar-min-01.png", ];
-var index = 0;
+var _top = null;
 
 function mOver(obj) {
     obj.className = "";
@@ -10,15 +8,21 @@ function mOut(obj) {
     obj.className = "blur3";
 }
 
-
 function forHeaderBackImg() {
-    var _headerObj = document.getElementById("index_header");
-    var currentImage = images[index];
-    _headerObj.style.backgroundImage = "url(" + currentImage + ")";
-    index++;
-    if (images[index] == null) {
-        index = 0;
+    var _backImgObj = $("#backImg");
+    _backImgObj.animate({
+        marginTop: "-710px"
+    }, 2000);
+    if (_top == null) {
+        _top = true;
+        _backImgObj.animate({
+            marginTop: "-710px"
+        }, 2000);
+    } else {
+        _top = null;
+        _backImgObj.animate({
+            marginTop: "0px"
+        }, 2000);
     }
 }
-
-setInterval("forHeaderBackImg()", 4000);
+setInterval("forHeaderBackImg()", 6000);
